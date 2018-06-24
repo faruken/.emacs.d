0b3a9a312cc13bc955f9ffdb0f29912ab866c0df
faruken <faruken@users.noreply.github.com>
2018-06-25 03:50:28 +1000

    natural titlebar

--- nickiminaj/base-config.el
+++ nickiminaj/base-config.el
@@ -1,3 +1,5 @@
 (set-frame-font "Fira Mono Medium for Powerline 18" nil t)
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
 
 (provide 'base-config)
