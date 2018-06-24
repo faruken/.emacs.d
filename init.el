d7fe6c3564d3f36b5c78d73ec692e52fe8cb2f81
faruken <faruken@users.noreply.github.com>
2018-06-25 03:33:12 +1000

    init.el

---
+++ init.el
@@ -0,0 +1,13 @@
+; Ayo, I been on, bitch, you been corn
+; Bentley tints on, Fendi prints on
+; I mean I been Storm, X-Men been formed
+; He keep on dialin' Nicki like the Prince song
+
+(add-to-list 'load-path (concat user-emacs-directory "nickiminaj"))
+
+(require 'base)
+(require 'base-config)
+(require 'base-theme)
+(require 'base-extensions)
+
+(require 'lang-python)
