0dd6d3236cc0fd8d3364adc96354a72d8fcb6261
faruken <faruken@users.noreply.github.com>
2018-06-25 19:12:41 +1000

    extensions

---
+++ nickiminaj/base-extensions.el
@@ -0,0 +1,85 @@
+(use-package avy
+  :bind
+  ("C-c SPC" . avy-goto-char))
+
+(use-package company
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package ediff
+  :config
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+  (setq-default ediff-highlight-all-diffs 'nil)
+  (setq ediff-diff-options "-w"))
+
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-copy-env "PYTHONPATH")
+    (exec-path-from-shell-initialize)))
+
+(use-package expand-region
+  :bind
+  ("C-=" . er/expand-region))
+
+(use-package flycheck)
+
+(use-package counsel
+  :bind
+  ("M-x" . counsel-M-x)
+  ("C-x C-m" . counsel-M-x)
+  ("C-x C-f" . counsel-find-file)
+  ("C-x c k" . counsel-yank-pop))
+
+(use-package counsel-projectile
+  :bind
+  ("C-x v" . counsel-projectile)
+  ("C-x c p" . counsel-projectile-ag)
+  :config
+  (counsel-projectile-on))
+
+(use-package ivy
+  :bind
+  ("C-x s" . swiper)
+  ("C-x C-r" . ivy-resume)
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers nil)
+  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
+
+(use-package hlinum
+  :config
+  (hlinum-activate))
+
+(use-package linum
+  :config
+  (setq linum-format " %3d ")
+  (global-linum-mode nil))
+
+(use-package neotree
+  :config
+  (setq neo-theme 'icons
+    neotree-smart-optn t
+    neo-window-fixed-size nil))
+
+(use-package all-the-icons)
+(use-package page-break-lines)
+(use-package smex)
+(use-package zoom
+  :config
+  (zoom-mode t))
+(use-package smartparens)
+
+
+(use-package windmove
+  :bind
+  ("C-x <up>" . windmove-up)
+  ("C-x <down>" . windmove-down)
+  ("C-x <left>" . windmove-left)
+  ("C-x <right>" . windmove-right))
+
+(use-package yasnippet
+  :config
+  (yas-global-mode 1))
+
+(provide 'base-extensions)
