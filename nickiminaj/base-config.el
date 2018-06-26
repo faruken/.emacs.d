86533dded9a492d8873e301b9cc0c5ac415cfb7a
faruken <faruken@users.noreply.github.com>
2018-06-27 03:38:17 +1000

    ignore zoom in neotree

--- nickiminaj/base-config.el
+++ nickiminaj/base-config.el
@@ -1,5 +1,6 @@
 (set-frame-font "Fira Mono Medium for Powerline 18" nil t)
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
+(custom-set-variables '(zoom-ignored-buffer-names '(" *NeoTree*")))
 
 (provide 'base-config)
